6c62c9cb9cb1dbea5b1bef101a9367afd1482fe9
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-02-04 22:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstChar(text) {
   // your code here
+	function firstChar(str) {
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str[i] !== " ") {
+            return str[i];
+        }
+
+    }
+
+    return "";
+}
+
 }
 
 // Do not change the code below
